9427d729fc805a31da7083a84ad9dfc8424fcfa9
Gustavo Eugênio <gustavo.eugenio@hotmail.com>
2023-09-22 00:37:53 -0300

    V2 cleaner notifications

--- script.py
+++ script.py
@@ -23,24 +23,36 @@ def check_availability(url) -> bool:
   if req.status_code != 200:
     print("Failed request.\n", req.status_code)
   soup = BeautifulSoup(req.content, 'html.parser')
-  disponible_ticket_types = soup.find_all(match_disponible_ticket)
-  for tag in disponible_ticket_types:
-    asyncio.run(send_telegram_notification(url, tag))
-  return len(disponible_ticket_types) > 0
+  disponible_sectors = soup.find_all(match_disponible_sector)
+  for tag in disponible_sectors:
+    (sector, ticket_types) = extract_data_from(tag)
+    asyncio.run(send_telegram_notification(url, sector, ticket_types))
+  return len(disponible_sectors) > 0
 
-def match_disponible_ticket(tag) -> bool:
-  if tag.has_attr('class') and 'ticket-type-item' in tag['class']:
-    if tag.find('div', string="Indisponível no momento"):
-      return False
-    else:
-      return True
+def match_disponible_sector(tag) -> bool:
+  if tag.name == 'form':
+    all_types = tag.find_all('div', 'ticket-type-item')
+    for type in all_types:
+      if not type.find('div', 'ticket-type-unavailable-sec'):
+        return True
   return False
 
-async def send_telegram_notification(url, ticket_tag):
+def extract_data_from(tag):
+  sector = tag.find('div', 'pc-list-category').contents[1].string
+  types = []
+  types_tag = tag.find_all('div', 'ticket-type-item')
+  for each in types_tag:
+    if not(each.find('div', 'ticket-type-unavailable-sec')):
+      types.append(each.find('div', 'ticket-type-title').contents[1].string)
+  return (sector, types)
+
+async def send_telegram_notification(url, sector, types):
   bot = telegram.Bot(token=config['botToken'])
-  # sector = ticket_tag.find_parent("form").find('div', 'pc-list-category').contents[1].string
-  # type = ticket_tag.find('div', 'ticket-type-title').string
-  await bot.send_message(config['channelId'], "Found a disponible ticket for {}.".format(url))
+  message = "Found a disponible ticket for {}.\n\n{}".format(url, sector) 
+  for type in types:
+    message += '\n - {}'.format(type)
+  await bot.send_message(config['channelId'], message)
+
   return
 
 if __name__ == '__main__':
