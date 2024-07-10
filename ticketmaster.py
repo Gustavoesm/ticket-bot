351f311eaf32061292c0d65c119fb2289e7731ed
Gustavo Eugênio <gustavo.eugenio@hotmail.com>
2024-07-09 22:32:06 -0300

    removed magic numbers

--- ticketmaster.py
+++ ticketmaster.py
@@ -7,12 +7,17 @@ def ticketmaster_scan(req):
   start = desired_tag.find("App.bootstrapData(")
   end = desired_tag.find("App.start();")
   data = json.loads(desired_tag[start+18:end-7])
-  available = [
-    [ticket for ticket in sector['rates'] if ticket['id'] == 280873] 
-    for sector 
-    in data['model']['data']['shows'][0]['sectors'] 
-    if sector['id'] == 56165
-  ][0][0]['available']
+  desired_pricing_id = 733056 # should recover from site
+  desired_sector_id = 118982 # should recover from site
+  try:
+    available = [
+      [ticket for ticket in sector['rates'] if ticket['id'] == desired_pricing_id] 
+      for sector 
+      in data['model']['data']['shows'][0]['sectors'] 
+      if sector['id'] == desired_sector_id
+    ][0][0]['available']
+  except:
+    return []
 
   if(available):
     message = "Encontrei um ingresso para um show que te interessa! 👍 {}.\n\nPISTA PREMIUM\n- Meia\n\nJá vai pensando em como me agradecer.".format(req.url)
